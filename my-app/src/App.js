288c0a254c25fc280d3ebffb4b89a4592ba183b4
EvaTu <evatuw@gmail.com>
2022-02-11 12:21:44 -0500

    Add Home Products db.json

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,14 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [home, setHome] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:3000/home")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  })
   return (
     <div className="App">
       
